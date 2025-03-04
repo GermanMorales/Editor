7ff9ae013bd5764b4d722950cb88173b669c6240
GermanMorales <germanricardomorales99@gmail.com>
2025-03-03 22:32:40 -0500

    fix catalogue title

--- src/app/components/catalogue.jsx
+++ src/app/components/catalogue.jsx
@@ -7,7 +7,7 @@ const Catalogue = ({ webData }) => {
   return (
     <div id="Catalogue" className="catalogue-main-div">
       <div className="catalogue-container">
-        <h1 className="catalogue-title">
+      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-primary">
           {webData.catalogo.titulo}
         </h1>
 
